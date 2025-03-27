b02242a4f1b806b97c05faed095009609356133e
Goldy T Joseph <35062235+goldytjoseph@users.noreply.github.com>
2025-03-27 14:29:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Change `your-repo-name` to your actual repository name
 export default defineConfig({
-  base: '/goldy-portfolio/',
   plugins: [react()],
-})
+  base: '/goldytjoseph.github.io/',  // <- Add this line
+});
