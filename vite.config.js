4c79b67dee2844a9b973bd18767f986db33bce4e
Goldy T Joseph <35062235+goldytjoseph@users.noreply.github.com>
2025-12-06 14:07:43 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Change `your-repo-name` to your actual repository name
-export default defineConfig({
-  plugins: [react()],
-  base: '/goldytjoseph.github.io/',  // <- Add this line
-});
